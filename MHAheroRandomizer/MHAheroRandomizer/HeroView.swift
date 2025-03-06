a26fc4edb657c5d2951cf373d26dce387e76247f
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-03-07 03:15:21 +0500

    Update HeroView.swift

--- MHAheroRandomizer/MHAheroRandomizer/HeroView.swift
+++ MHAheroRandomizer/MHAheroRandomizer/HeroView.swift
@@ -13,119 +13,106 @@ struct HeroView: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 10) {
-                // Изображение героя
-                AsyncImage(url: URL(string: hero.images.lg)) { image in
-                    image
-                        .resizable()
-                        .frame(width: 300, height: 300)
-                        .cornerRadius(10)
-                        .shadow(radius: 5)
-                        .clipShape(Circle())
-                                                          
-                } placeholder: {
-                    ProgressView()
-                }
+                HeroImageView(imageURL: hero.images.lg)
 
-                // Имя героя
                 Text(hero.name)
                     .font(.title)
                     .fontWeight(.bold)
                     .padding(.top, 5)
 
-                // Биография
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Full Name: \(hero.biography.fullName ?? "Unknown")")
-                        .font(.headline)
-
-                    Text("First Appearance: \(hero.biography.firstAppearance ?? "Unknown")")
-                        .font(.headline)
-                    
-                    Text("Alter Egos: \(hero.biography.alterEgos ?? "Unknown")")
-                        .font(.headline)
-                    
-                    Text("Place of Birth: \(hero.biography.placeOfBirth ?? "Unknown")")
-                        .font(.headline)
-
-                    Text("Publisher: \(hero.biography.publisher ?? "Unknown")")
-                        .font(.headline)
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color.blue.opacity(0.1))
-                .cornerRadius(10)
-                
-                HStack {
-                    // Силовые характеристики
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Powerstats:")
-                            .font(.headline)
-
-                        Text("💪 Strength: \(hero.powerstats.strength)")
-                        Text("⚡ Speed: \(hero.powerstats.speed)")
-                        Text("🧠 Intelligence: \(hero.powerstats.intelligence)")
-                        Text("🔋 Durability: \(hero.powerstats.durability)")
-                        Text("🔥 Power: \(hero.powerstats.power)")
-                        Text("🥊 Combat: \(hero.powerstats.combat)")
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.purple.opacity(0.1))
-                    .cornerRadius(10)
-
-                    // Внешность
-                    VStack(alignment: .leading, spacing: 5) {
-                        Text("Appearance:")
-                            .font(.headline)
-
-                        Text("👤 Gender: \(hero.appearance.gender)")
-                        Text("🧬 Race: \(hero.appearance.race ?? "Unknown")")
-                        Text("📏 Height: \(hero.appearance.height[1])") // 203 cm
-                        Text("⚖️ Weight: \(hero.appearance.weight[1])") // 441 kg
-                        Text("👁 Eye Color: \(hero.appearance.eyeColor)")
-                        Text("💇 Hair Color: \(hero.appearance.hairColor)")
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color.green.opacity(0.1))
-                    .cornerRadius(10)
-                    
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                
-                
-                // Работа
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Work:")
-                        .font(.headline)
-
-                    Text("💼 Occupation: \(hero.work.occupation ?? "Unknown")")
-                    Text("📍 Base: \(hero.work.base ?? "Unknown")")
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color.pink.opacity(0.1))
-                .cornerRadius(10)
-                
-                //Семья
-                VStack(alignment: .leading, spacing: 5) {
-                    Text("Connections:")
-                        .font(.headline)
-
-                    Text("👥 Group Affiliation: \(hero.connections.groupAffiliation ?? "None")")
-                    Text("👨‍👩‍👧‍👦 Relatives: \(hero.connections.relatives ?? "Unknown")")
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .background(Color.orange .opacity(0.1))
-                .cornerRadius(10)
-                
-                
+                BiographyView(biography: hero.biography)
+                AppearanceView(appearance: hero.appearance)
+                WorkView(work: hero.work)
+                ConnectionsView(connections: hero.connections)
             }
             .padding()
         }
+    }
+}
+
+
+struct HeroImageView: View {
+    let imageURL: String
+
+    var body: some View {
+        AsyncImage(url: URL(string: imageURL)) { image in
+            image.resizable()
+        } placeholder: {
+            ProgressView()
+        }
+        .frame(width: 300, height: 300)
+        .cornerRadius(10)
+        .shadow(radius: 5)
+        .clipShape(Circle())
+    }
+}
+
+struct BiographyView: View {
+    let biography: Biography
+
+    var body: some View {
+        InfoCardView(title: "Biography", content: [
+            "Full Name: \(biography.fullName ?? "Unknown")",
+            "First Appearance: \(biography.firstAppearance ?? "Unknown")",
+            "Place of Birth: \(biography.placeOfBirth ?? "Unknown")",
+            "Publisher: \(biography.publisher ?? "Unknown")"
+        ], color: Color.blue.opacity(0.1))
+    }
+}
+
+struct AppearanceView: View {
+    let appearance: Appearance
+
+    var body: some View {
+        InfoCardView(title: "Appearance", content: [
+            "👤 Gender: \(appearance.gender)",
+            "🧬 Race: \(appearance.race ?? "Unknown")",
+//            "📏 Height: \(appearance.height.last ?? "Unknown")",
+//            "⚖️ Weight: \(appearance.weight.last ?? "Unknown")",
+            "👁 Eye Color: \(appearance.eyeColor)",
+            "💇 Hair Color: \(appearance.hairColor)"
+        ], color: Color.green.opacity(0.1))
+    }
+}
+
+struct WorkView: View {
+    let work: Work
+
+    var body: some View {
+        InfoCardView(title: "Work", content: [
+            "💼 Occupation: \(work.occupation ?? "Unknown")",
+            "📍 Base: \(work.base ?? "Unknown")"
+        ], color: Color.pink.opacity(0.1))
+    }
+}
+
+struct ConnectionsView: View {
+    let connections: Connections
+
+    var body: some View {
+        InfoCardView(title: "Connections", content: [
+            "👥 Group Affiliation: \(connections.groupAffiliation ?? "None")",
+            "👨‍👩‍👧‍👦 Relatives: \(connections.relatives ?? "Unknown")"
+        ], color: Color.orange.opacity(0.1))
+    }
+}
+
+
+struct InfoCardView: View {
+    let title: String
+    let content: [String]
+    let color: Color
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text(title)
+                .font(.headline)
+            ForEach(content, id: \.self) { Text($0) }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+        .background(color)
+        .cornerRadius(10)
     }
 }
 
